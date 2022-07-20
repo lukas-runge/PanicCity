63e8be9d4deba38dfae5b1d0287d64ca07af5ca3
Lukas Runge <login@lukasrunge.de>
2022-07-21 00:34:37 +0200

    💄 changed order of functions

--- Assets/Scripts/movementController.cs
+++ Assets/Scripts/movementController.cs
@@ -26,16 +26,6 @@ public class movementController : MonoBehaviour {
 		desiredSpeed = desiredSpeed + Random.Range(-0.4f, 0.4f);
 	}
 
-	void OnTriggerEnter2D(Collider2D col) {
-		if (col.gameObject.tag == "Door") {
-			doorReached = true;
-		}
-
-		if (col.gameObject.tag == "Sammelplatz") {
-			Destroy(gameObject); // despawn human
-		}
-	}
-
 	void FixedUpdate() {
 		if (State.runningState == false) { // simulation stopped
 			return; // do nothing
@@ -44,6 +34,16 @@ public class movementController : MonoBehaviour {
 		//get new list of forces from MathProcessor and apply them to rb
 		foreach (var force in Math.getForces(gameObject)) {
 			rb.AddForce(force);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "Door") {
+			doorReached = true;
+		}
+
+		if (col.gameObject.tag == "Sammelplatz") {
+			Destroy(gameObject); // despawn human
 		}
 	}
 }
