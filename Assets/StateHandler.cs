a1fcb71db251d796338661809ee3f121173b1d7f
Lukas Runge <login@lukasrunge.de>
2022-07-20 22:52:40 +0200

    ⚡💄 refactored StateHandler for easier explanation

--- Assets/StateHandler.cs
+++ Assets/StateHandler.cs
@@ -1,46 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-/// Include the name space for TextMesh Pro
 using TMPro;
 
-public class StateHandler : MonoBehaviour
-{
+public class StateHandler : MonoBehaviour {
+    // "global" state boolean
     public bool runningState = false;
+
+    // reference to canvas objects
     public Slider slider1;
     public Slider slider2;
     public Slider slider3;
     public Slider slider4;
     public Toggle toggle1;
-    public TimerController Timer;
     public TMP_Text ButtonTextObject;
 
-    public void toggleRunning()
-    {
-        if (!runningState)
-        {
-            Debug.Log("Starting");
+    // reference to TimerController
+    public TimerController Timer;
+
+    public void toggleRunning() { // state mutation function
+        if (!runningState) { // start simulation
             runningState = true;
-            ButtonTextObject.text = "RESET";
-            Debug.Log("Running");
             Timer.startTimer();
-        } else {
-            Debug.Log("Stopping");
+            ButtonTextObject.text = "RESET";
+        } else { // stop simulation
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }    
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
